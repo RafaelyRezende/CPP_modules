5f8b4b040622ff98e1c9dabb24f429b4840c2134
Rafael Luis Yanase de Rezende <rafael.yanase@gmail.com>
2026-04-20 18:50:41 +0100

    Update: partial implementation

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+void	copy_and_replace(const char *source_path, const char *pattern, const char *subs) {
+	if (!source_path) {
+		std::cerr << "Error: NULL filename" << std::endl;
+		exit(1);
+	}
+	std::ifstream	infile(source_path);
+	if (!infile) {
+		std::cerr << "Error: cannot open file.\n";
+		exit(2);
+	}
+	std::string		dest_path = std::string(source_path) + ".replace";
+	std::ofstream	outfile(dest_path.c_str());
+	if (!outfile) {
+		std::cerr << "Error: cannot write to file\n";
+		exit(3);
+	}
+}
 
 int	main(int argc, char **argv)
 {
-	if (argc != 
+	if (argc != 4) {
+		std::cerr << "Error: wrong number of arguments" << std::endl;
+		return (1);
+	}
+	copy_and_replace(argv[1], argv[2], argv[3]);
 }
